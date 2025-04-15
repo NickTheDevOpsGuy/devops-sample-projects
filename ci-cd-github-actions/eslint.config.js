dd5898d713885ba14064fe31811bc87163e98670
Nicholas Clark <79878122+NickTheDevOpsGuy@users.noreply.github.com>
2025-04-15 11:54:33 -0400

    Create eslint.config.js

---
+++ ci-cd-github-actions/eslint.config.js
@@ -0,0 +1,16 @@
+import js from '@eslint/js';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    rules: {
+      'no-console': 'warn',
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    },
+  },
+];
