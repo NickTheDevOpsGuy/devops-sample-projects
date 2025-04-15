7f77d965875b0287ac1f29fdd4a01bdbbc103ef2
Nicholas Clark <79878122+NickTheDevOpsGuy@users.noreply.github.com>
2025-04-15 11:37:27 -0400

    Update eslint.config.js

--- .github/workflows/eslint.config.js
+++ .github/workflows/eslint.config.js
@@ -1,42 +1,28 @@
-import js from '@eslint/js';
-import node from 'eslint-plugin-n';
-import eslintPluginImport from 'eslint-plugin-import';
-import globals from 'globals';
-
-export default [
-  js.configs.recommended,
-  {
-    files: ['**/*.js'],
-    languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-    },
-    plugins: {
-      import: eslintPluginImport,
-      node,
-    },
-    rules: {
-      // General JavaScript best practices
-      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-      'no-console': 'warn',
-      'eqeqeq': ['error', 'always'],
-      
-      // Node-specific
-      'n/no-deprecated-api': 'warn',
-
-      // Import rules
-      'import/order': [
-        'warn',
-        {
-          groups: ['builtin', 'external', 'internal'],
-          pathGroupsExcludedImportTypes: ['builtin'],
-          alphabetize: { order: 'asc', caseInsensitive: true },
-        },
-      ],
-    },
+module.exports = {
+  env: {
+    node: true,
+    es2022: true,
+    jest: true,
   },
-];
+  extends: ['eslint:recommended', 'plugin:import/recommended'],
+  plugins: ['import'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    // Basic best practices
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'no-console': 'warn',
+    'eqeqeq': ['error', 'always'],
+
+    // Import organization
+    'import/order': [
+      'warn',
+      {
+        groups: ['builtin', 'external', 'internal'],
+        alphabetize: { order: 'asc', caseInsensitive: true },
+      },
+    ],
+  },
+};
